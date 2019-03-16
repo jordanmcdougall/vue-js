06dca901fd27a10cfdce8c518664453230d3c05a
Jordan McDougall <jordanmcdougall@hotmail.co.uk>
2019-03-16 15:00:41 +0000

    Added Multiple Instances Tutorial

---
+++ 2. Multiple Instances/app.js
@@ -0,0 +1,36 @@
+var one = new Vue({
+    el:'#vue-app-one',
+    data:{
+        title: 'Vue App One'
+
+    },
+    methods:{
+    },
+    computed:{
+        greet: function(){
+            return 'Hello from app 1';
+        }
+    }
+});
+
+var two = new Vue({
+    el:'#vue-app-two',
+    data:{
+        title: 'Vue App Two'
+
+
+    },
+    methods:{
+        changeTitle: function(){
+            one.title = 'Title Changed'
+        }
+    },
+    computed:{
+        greet: function(){
+            return 'Hello from app 2';
+        }
+    }
+});
+
+
+two.title = 'Changed from outside';
